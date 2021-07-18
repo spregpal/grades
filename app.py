8ca41bb4bcdd2365620d0c7e67276f2ba3dc62e7
Vish Pallem <vishpallem@Vishs-MacBook-Pro.local>
2021-07-18 15:44:15 -0400

    Vishruth's Changes

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+@from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Flask with docker!'
+@app.route('/getgrades')
+def getgrades():
+    return 'Grade is 100!'
