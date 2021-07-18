2cc5d231d0752048789975d5e3819ed6eee78e6a
Vish Pallem <vishpallem@Vishs-MacBook-Pro.local>
2021-07-18 16:05:53 -0400

    NewCode

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-@from flask import Flask
+
+from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Flask with docker!'
 @app.route('/getgrades')
-def getgrades():
-    return 'Grade is 100!'
+def home2():
+    return 'Flask with docker 2.0!'
